e564357be17a5520d42c12fab95158d2c9932021
mbluis4 <92177912+mbluis4@users.noreply.github.com>
2022-06-11 12:03:27 -0300

    reviews

--- index.js
+++ index.js
@@ -6,66 +6,34 @@ let slidePos = 0;
 
 let totalSlides = slides.length;
 
-// my solution
-/** 
-let moveToPrevSlide = () => {
-  if (slidePos > 0) {
-    slides[slidePos].className = "carousel-item";
-    slidePos -= 1;
-    slides[slidePos].className = "carousel-item carousel-item-visible";
-  } else {
-    slides[slidePos].className = "carousel-item";
-    slidePos = totalSlides - 1;
-    slides[slidePos].className = "carousel-item carousel-item-visible";
+let hideAllSlides = () => {
+  for (let slide of slides) {
+    slide.classList.remove("carousel-item-visible");
+    slide.classList.add("carousel-item-hidden");
   }
 };
 
 let moveToNextSlide = () => {
-  if (slidePos < totalSlides - 1) {
-    slides[slidePos].className = "carousel-item";
-    slidePos += 1;
-    slides[slidePos].className = "carousel-item carousel-item-visible";
-  } else {
-    slides[slidePos].className = "carousel-item";
+  hideAllSlides();
+  if (slidePos === totalSlides - 1) {
     slidePos = 0;
-    slides[slidePos].className = "carousel-item carousel-item-visible";
+  } else {
+    slidePos++;
   }
-}; 
+  slides[slidePos].classList.add("carousel-item-visible");
+};
 
-*/
-  // Another aproach
-
-  let hideAllSlides = () => {
-      for (let slide of slides) {
-          
-           slide.classList.remove("carousel-item-visible")
-           slide.classList.add("carousel-item-hidden")
-           
-      }
+let moveToPrevSlide = () => {
+  hideAllSlides();
+  if (slidePos === 0) {
+    slidePos = totalSlides - 1;
+  } else {
+    slidePos--;
   }
+  slides[slidePos].classList.add("carousel-item-visible");
+};
 
-  let moveToNextSlide = () => {
-      hideAllSlides()
-      if (slidePos === totalSlides-1) {
-          slidePos = 0
-      } else {
-          slidePos++
-      }
-      slides[slidePos].classList.add("carousel-item-visible")
-  }
-
-
-  let moveToPrevSlide = () => {
-      hideAllSlides()
-      if (slidePos === 0) {
-          slidePos = totalSlides-1 
-      } else {
-          slidePos--
-      }
-      slides[slidePos].classList.add("carousel-item-visible")  
-    }
-
-  document
+document
   .getElementById("carousel-btn-prev")
   .addEventListener("click", moveToPrevSlide);
 
